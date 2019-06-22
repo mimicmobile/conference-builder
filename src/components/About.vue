0f3204368127444ea913dc92e023cfbe6193d4af
Jeff Corcoran <jcorcoran+github@gmail.com>
2019-06-22 17:20:25 -0400

    Add Links

--- src/components/About.vue
+++ src/components/About.vue
@@ -12,9 +12,8 @@
       <v-layout wrap>
         <v-flex class="py-3" xs12>
           <div class="header-image-cont">
-            <img :src="header_image" alt class="header-image" v-if="header_image"/>
+            <img :src="headerImageOrPlaceholder" alt class="header-image"/>
             <div class="header-text-cont">
-              <span v-if="!header_image">&lt; Header Placeholder &gt;</span>
               <v-progress-circular color="accent" indeterminate v-if="isHeaderLoading"></v-progress-circular>
             </div>
           </div>
@@ -36,7 +35,7 @@
           <v-text-field append-icon="fas fa-code" label="Website" outline v-model="website"></v-text-field>
         </v-flex>
         <v-flex class="pb-3" md6 xs12>
-          <v-text-field append-icon="email" label="Email" outline v-model="contact_email"></v-text-field>
+          <v-text-field append-icon="email" label="Email" outline v-model="contactEmail"></v-text-field>
         </v-flex>
 
         <v-flex class="py-3" text-xs-left xs12>
@@ -44,19 +43,19 @@
         </v-flex>
 
         <v-flex class="pb-3" md6 xs12>
-          <v-text-field append-icon="fas fa-marker" label="Name" outline v-model="venue.name"></v-text-field>
+          <v-text-field append-icon="fas fa-marker" label="Name" outline v-model="venueName"></v-text-field>
         </v-flex>
         <v-flex class="pb-3" md6 xs12>
-          <v-text-field append-icon="fas fa-building" label="Address" outline v-model="venue.address"></v-text-field>
+          <v-text-field append-icon="fas fa-building" label="Address" outline v-model="venueAddress"></v-text-field>
         </v-flex>
-        <v-flex class="pb-3 venue-image" md6 xs12>
-          <div class="venue-image-cont">
-            <img :src="venue.image_path" alt/>
-            <div class="venue-text-cont">
+        <v-flex class="pb-3 upload-image" md8 xs12>
+          <div class="upload-image-cont">
+            <img :src="venueImageOrPlaceholder" alt/>
+            <div class="upload-image-text-cont">
               <v-progress-circular color="accent" indeterminate v-if="isVenueImageLoading"></v-progress-circular>
             </div>
           </div>
-          <upload-btn @file-update="updateVenueImage" class="venue-image-btn" color="accent" noTitleUpdate
+          <upload-btn @file-update="updateVenueImage" class="upload-image-btn" color="accent" noTitleUpdate
                       round title="Set venue image">
             <template slot="icon-left">
               <v-icon>add</v-icon>
@@ -70,11 +69,70 @@
 
         <v-flex class="py-3" xs12>
           <v-card flat>
-            <v-card-title>
-              <span class="title font-weight-light">Group name</span>
-            </v-card-title>
+            <v-flex class="link-group" v-if="links.length > 0" wrap xs12>
+              <v-flex :key="link.name" class="link-group-item" lg3 md6 v-for="(link, index) in links" xs6>
+                <a :href="link.imagePath" target="_blank"><img :src="link.imagePath" alt/></a>
+                <span>{{ link.name }}</span>
+                <span><a :href="link.website" target="_blank">{{ link.website }}</a></span>
+                <span>{{ linkTypeNameFromId(link.typeId) }}</span>
+                <div class="link-group-button-cont">
+                  <v-btn @click="editLink(index)" color="secondary" round>Edit link
+                  </v-btn>
+                  <v-btn @click="deleteLink(index)" color="secondary" round>Delete link
+                  </v-btn>
+                </div>
+              </v-flex>
+            </v-flex>
+            <v-flex wrap xs12>
+              <v-btn @click="showNewLinkDialog" color="accent" round>Add new link</v-btn>
+            </v-flex>
           </v-card>
         </v-flex>
+
+        <v-dialog max-width="600px" v-model="newLinkDialog">
+          <v-card>
+            <v-card-title>
+              <span class="headline">Add a new link</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-layout wrap>
+                  <v-flex lg6 xs12>
+                    <v-text-field label="Name" required v-model="newLinkName"></v-text-field>
+                  </v-flex>
+                  <v-flex lg6 xs12>
+                    <v-text-field label="Website" required v-model="newLinkWebsite"></v-text-field>
+                  </v-flex>
+                  <v-flex lg6 xs12>
+                    <v-combobox :items="linkTypes" item-text="name" item-value="id" label="Type" outline small
+                                v-model="newLinkTypeId"></v-combobox>
+                  </v-flex>
+                  <v-flex class="pb-3 upload-image" lg12 xs12>
+                    <div class="upload-image-cont">
+                      <img :src="newLinkImageOrPlaceholder" alt/>
+                      <div class="upload-image-text-cont">
+                        <v-progress-circular color="accent" indeterminate
+                                             v-if="isNewLinkImageLoading"></v-progress-circular>
+                      </div>
+                    </div>
+                    <upload-btn @file-update="updateNewLinkImage" class="upload-image-btn" color="accent" noTitleUpdate
+                                round title="Set image">
+                      <template slot="icon-left">
+                        <v-icon>add</v-icon>
+                      </template>
+                    </upload-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn @click="newLinkDialog = false" color="secondary darken-1" flat>Close</v-btn>
+              <v-btn :disabled="!validNewLink" @click="addNewLink" color="secondary darken-1" flat>{{ addOrSaveLink }}
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 
         <v-snackbar :timeout="timeout" color="primary" v-model="isModified">
           Modified
@@ -105,6 +163,10 @@
 
   import UploadButton from "vuetify-upload-button"
 
+  function getPlaceholderImage (dimension) {
+    return `https://via.placeholder.com/${dimension}/454545/454545`
+  }
+
   export default {
     name: "About",
     props: ["loadedId"],
@@ -117,16 +179,23 @@
       isLoading: true,
       isHeaderLoading: false,
       isVenueImageLoading: false,
-      header_image: false,
+      isNewLinkImageLoading: false,
+      headerImage: false,
       description: null,
       twitter: null,
       website: null,
-      contact_email: null,
-      venue: {
-        name: null,
-        address: null,
-        image_path: null
-      },
+      contactEmail: null,
+      venueName: null,
+      venueAddress: null,
+      venueImagePath: null,
+      newLinkIndex: null,
+      newLinkName: null,
+      newLinkWebsite: null,
+      newLinkImagePath: null,
+      newLinkTypeId: null,
+      newLinkDialog: false,
+      links: [],
+      linkTypes: [],
       currentId: null,
       currentIdUpdated: false,
       modified: false,
@@ -137,6 +206,21 @@
     computed: {
       isModified () {
         return this.modified && !this.snapshotIsOld
+      },
+      headerImageOrPlaceholder () {
+        return this.headerImage || getPlaceholderImage("800x260")
+      },
+      venueImageOrPlaceholder () {
+        return this.venueImagePath || getPlaceholderImage("200x200")
+      },
+      newLinkImageOrPlaceholder () {
+        return this.newLinkImagePath || getPlaceholderImage("200x200")
+      },
+      validNewLink () {
+        return this.newLinkName != null && this.newLinkWebsite != null && this.newLinkImagePath != null && this.newLinkTypeId != null
+      },
+      addOrSaveLink () {
+        return this.newLinkIndex != null ? "Save" : "Add"
       }
     },
     methods: {
@@ -147,14 +231,89 @@
           .get()
           .then((aboutCollection) => this.loadCollection(aboutCollection))
       },
+      showNewLinkDialog () {
+        this.newLinkName = null
+        this.newLinkWebsite = null
+        this.newLinkImagePath = null
+        this.newLinkTypeId = null
+        this.newLinkIndex = null
+        this.newLinkDialog = true
+      },
+      addNewLink () {
+        let fixedTypeId
+        if (typeof this.newLinkTypeId === "string" || this.newLinkTypeId instanceof String) {
+          fixedTypeId = this.newLinkTypeId.replace(" ", "_").toLowerCase()
+          this.linkTypes.push({
+            name: this.newLinkTypeId,
+            id: fixedTypeId
+          })
+        } else {
+          fixedTypeId = this.newLinkTypeId["id"]
+        }
+
+        const update = {
+          name: this.newLinkName,
+          website: this.newLinkWebsite,
+          imagePath: this.newLinkImagePath,
+          typeId: fixedTypeId
+        }
+
+        if (this.newLinkIndex != null) {
+          this.links[this.newLinkIndex] = update
+        } else {
+          this.links.push(update)
+        }
+        this.cleanUpLinkTypes()
+
+        this.newLinkDialog = false
+        this.changed()
+      },
+      editLink (index) {
+        const link = this.links[index]
+
+        this.newLinkIndex = index
+        this.newLinkName = link.name
+        this.newLinkWebsite = link.website
+        this.newLinkTypeId = this.linkTypeFromId(link.typeId)
+        this.newLinkImagePath = link.imagePath
+        this.newLinkDialog = true
+      },
+      deleteLink (index) {
+        this.links.splice(index, 1)
+        this.cleanUpLinkTypes()
+        this.changed()
+      },
+      cleanUpLinkTypes () {
+        const newLinkTypes = new Set([])
+        this.links.forEach((link) => this.linkTypes.forEach((linkType) => {
+          if (link.typeId === linkType.id) newLinkTypes.add(linkType)
+        }))
+        this.linkTypes = Array.from(newLinkTypes)
+        this.sortLinkTypes()
+      },
+      sortLinkTypes () {
+        this.linkTypes.sort((a, b) => a.name - b.name)
+      },
+      linkTypeFromId (linkId) {
+        return this.linkTypes.filter((linkType) => linkType.id === linkId)[0]
+      },
+      linkTypeNameFromId (linkId) {
+        return this.linkTypeFromId(linkId).name
+      },
       save () {
         let update = {
-          header_image: this.header_image,
+          header_image: this.headerImage,
           description: this.description,
           twitter: this.twitter,
           website: this.website,
-          contact_email: this.contact_email,
-          venue: this.venue,
+          contact_email: this.contactEmail,
+          venue: {
+            name: this.venueName,
+            address: this.venueAddress,
+            image_path: this.venueImagePath
+          },
+          links: this.links,
+          link_types: this.linkTypes,
           created: firebase.firestore.FieldValue.serverTimestamp(),
           author: {
             displayName: firebase.auth().currentUser.displayName,
@@ -179,44 +338,41 @@
         this.modified = true
       },
       updateState (d) {
-        this.header_image = d.header_image
+        this.headerImage = d.header_image
         this.description = d.description
         this.twitter = d.twitter
         this.website = d.website
-        this.contact_email = d.contact_email
-        this.venue = d.venue
+        this.contactEmail = d.contact_email
+        this.venueName = d.venue.name
+        this.venueAddress = d.venue.address
+        this.venueImagePath = d.venue.image_path
+        this.links = d.links || []
+        this.linkTypes = d.link_types || []
+        this.sortLinkTypes()
+      },
+      updateImage (file, path, urlVar, loadingVar) {
+        this[loadingVar] = true
+
+        firebase.storage().ref().child(`images/${path}${file.name}`)
+          .put(file)
+          .then((snapshot) => {
+            snapshot.ref.getDownloadURL()
+              .then((url) => {
+                this[urlVar] = url
+                this[loadingVar] = false
+
+                this.changed()
+              })
+          })
       },
       updateHeaderImage (file) {
-        let d = this
-        this.isHeaderLoading = true
-
-        firebase.storage().ref().child(`images/${file.name}`)
-          .put(file)
-          .then((snapshot) => {
-            snapshot.ref.getDownloadURL()
-              .then((url) => {
-                d.header_image = url
-                d.isHeaderLoading = false
-
-                this.changed()
-              })
-          })
+        this.updateImage(file, "header/", "headerImage", "isHeaderLoading")
       },
       updateVenueImage (file) {
-        let d = this
-        this.isVenueImageLoading = true
-
-        firebase.storage().ref().child(`images/venue/${file.name}`)
-          .put(file)
-          .then((snapshot) => {
-            snapshot.ref.getDownloadURL()
-              .then((url) => {
-                d.venue.image_path = url
-                this.isVenueImageLoading = false
-
-                this.changed()
-              })
-          })
+        this.updateImage(file, "venue/", "venueImagePath", "isVenueImageLoading")
+      },
+      updateNewLinkImage (file) {
+        this.updateImage(file, "links/", "newLinkImagePath", "isNewLinkImageLoading")
       },
       loadSnapshot (snap) {
         if (this.currentId !== null && this.currentId !== snap.id && this.modified) {
@@ -295,16 +451,18 @@
     max-height: 300px;
   }
 
-  .venue-image {
+  .upload-image {
     display: flex;
+    flex-flow: row;
+    align-items: center;
   }
 
-  .venue-image-btn {
+  .upload-image-btn {
     max-width: 40%;
   }
 
-  .venue-image-cont {
-    width: auto;
+  .upload-image-cont {
+    width: 320px;
     height: auto;
     min-height: 50%;
     min-width: 50%;
@@ -317,7 +475,7 @@
     border-radius: 4px;
   }
 
-  .venue-text-cont {
+  .upload-image-text-cont {
     width: auto;
     height: auto;
     display: flex;
@@ -326,18 +484,52 @@
     position: absolute;
   }
 
-  .venue-image div:hover {
+  .upload-image div:hover {
     border-color: #FFF;
   }
 
-  .venue-image img {
+  .upload-image img {
     width: 100%;
     height: 100%;
+  }
+
+  .link-group {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 300px);
+    grid-row-gap: 20px;
+    justify-content: space-around;
+  }
+
+  .link-group-item {
+    max-width: 320px;
+    display: flex;
+    flex-flow: column;
+    flex-basis: available;
+    text-align: start;
+    margin: 10px;
+  }
+
+  .link-group-item img {
+    width: 100%;
+    height: auto;
+  }
+
+  .link-group-item span {
+    font-size: 14pt;
+    padding: 4px;
+  }
+
+  .link-group-button-cont {
+    align-self: center
   }
 
   @media screen and (max-width: 800px) {
     .header-image-cont {
       min-width: auto !important;
+    }
+
+    .upload-image {
+      flex-flow: column;
     }
   }
 </style>
