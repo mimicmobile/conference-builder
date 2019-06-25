b0a8c3cee31d7841700c4a271e670773cc2eb85f
Jeff Corcoran <jcorcoran+github@gmail.com>
2019-06-25 13:42:07 -0400

    Vuetify tree shaking support

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin")
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new VuetifyLoaderPlugin()
+    ]
+  },
   devServer: {
     host: "0.0.0.0",
     port: 8080,
